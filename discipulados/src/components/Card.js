02f93293b04d33caec9224b1472949b7238df89c
avilapro <mejiasavilaeduardo@gmail.com>
2020-04-07 00:30:33 -0400

    cap 10 agregar css a los componentes

--- discipulados/src/components/Card.js
+++ discipulados/src/components/Card.js
@@ -1,16 +1,21 @@
 import React from 'react'
 import discipuladoImg from '../images/mariposa1.png'
+import './styles/Card.css'
 
 class Card extends React.Component {
   render(){
     return(
-      <div>
-        <div>
-          <img src={discipuladoImg} />
-        </div>
-        <div>
-          <h1>Guias Practicas</h1>
-          <p>Aprende las practicas fundamentales del discipulo</p>
+      <div className="card mx-auto Discipulos-Card" >
+        <div className="card-body" >
+          <div className="row center">
+            <div className="col-6">
+              <img src={discipuladoImg} className="float-right" id="Discipulos-img" />
+            </div>
+            <div className="col-6 Discipulos-Card-Info" >
+              <h1>Guias Practicas</h1>
+              <p>Aprende las practicas fundamentales del discipulo</p>
+            </div>
+          </div>
         </div>
       </div>
     )
--- discipulados/src/index.js
+++ discipulados/src/index.js
@@ -9,7 +9,7 @@ container.appendChild(element) */
 import React from 'react'
 import ReactDOM from 'react-dom'
 import Card from './components/Card'
-
+import 'bootstrap/dist/css/bootstrap.css'
 
 
 const container = document.getElementById('root')
